5a5e6b0ea27e1be0104219f9509e50b890fdd2ed
arash-malekzadeh <arash.malekzade@gmail.com>
2024-08-02 18:11:55 +0330

    Questions and result table

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,26 +1,32 @@
 import React, { useState } from "react";
+import data from "./database/data";
 
 export default function Questions() {
   const [checkes, setchecked] = useState(undefined);
+  const question = data[0];
   function onSelect() {
     console.log("radio button change");
   }
   return (
     <div className="questions">
-      <h2 className="text-light">نمونه سوال اول</h2>
+      <h2 className="text-light">{question.question}</h2>
 
-      <ul>
-        <li>
-          <input
-            type="radio"
-            value={false}
-            name="options"
-            id="q1-option"
-            onChange={onSelect}
-          />
-          <label className="text-primary" htmlFor={`q1-option`}></label>
-          <div className="check  checked"></div>
-        </li>
+      <ul key={question.id}>
+        {question.options.map((q, i) => (
+          <li key={i}>
+            <input
+              type="radio"
+              value={false}
+              name="options"
+              id={`q${i}-option`}
+              onChange={onSelect}
+            />
+            <label className="text-primary" htmlFor={`q${i}-option`}>
+              {q}
+            </label>
+            <div className="check  "></div>
+          </li>
+        ))}
       </ul>
     </div>
   );
